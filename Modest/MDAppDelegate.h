5526c07fa7f88e64566f152783d9f5478b13f01b
Josep Llodrà Grimalt <jlg.hrtc@gmail.com>
2014-03-22 18:21:53 +0100

    Initial Commit

---
+++ Modest/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  Modest
+//
+//  Created by Josep Llodrà Grimalt on 22/03/14.
+//  Copyright (c) 2014 Atlantis of code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Modest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Modest
+//
+//  Created by Josep Llodrà Grimalt on 22/03/14.
+//  Copyright (c) 2014 Atlantis of code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
